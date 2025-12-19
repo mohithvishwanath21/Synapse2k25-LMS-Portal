46aa64a9dbd0ab3220151c595c6ff72900c12a3b
Mohith Vishwanath <mohithmpc21@gmail.com>
2025-12-19 20:16:25 +0530

    Fix import case for QuizApproval

--- client/src/pages/admin/QuizApproval/Index.jsx
+++ client/src/pages/admin/QuizApproval/Index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { useGetPendingQuizzesQuery, useGetAllQuizzesQuery } from '@/services/AdminApi/adminQuizApi'
+import { useGetPendingQuizzesQuery, useGetAllQuizzesQuery } from '@/services/adminApi/adminQuizApi'
 import QuizPublishRequests from '../Notification/components/QuizPublishRequests' // We'll create this
 import QuizList from './components/QuizList' // We'll create this
 import LoadingSpinner from '@/components/FallbackUI/LoadingSpinner'
